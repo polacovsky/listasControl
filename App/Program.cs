b7cbf95e0f1498efb9d91df78c41d66038a06740
Jose <joseluiszaratebarraza@hotmail.com>
2014-10-02 17:56:07 -0500

    OK

--- App/Program.cs
+++ App/Program.cs
@@ -2,25 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-
 namespace App
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
-           // Application.Run(new frmPrincipal());
+            Application.Run(new frmLogin());     
             if (CapaConexion.Usuario.Logueado) {
                 Application.Run(new frmPrincipal());
             }
-
         }
     }
 }
